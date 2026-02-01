45dd4ef4c8ca54cc486bf2d46ffb907b99e815d8
Nandan S.R <[srnandan.44@gmail.com]>
2026-02-01 15:00:25 +0530

    Implement Proof-of-Work mining simulator using SHA-256

---
+++ miner.py
@@ -0,0 +1,44 @@
+import hashlib
+import time
+
+# Block data (simulated)
+block_data = "Hello, this is my first Bitcoin mining project"
+
+# Difficulty = number of leading zeros required
+difficulty = 5
+
+start_time = time.time()
+nonce = 0
+attempts = 0
+
+
+print("Mining started")
+print("Difficulty:", difficulty)
+
+try:
+    while True:
+        text = block_data + str(nonce)
+        hash_result = hashlib.sha256(text.encode()).hexdigest()
+        attempts += 1
+
+
+        if hash_result.startswith("0" * difficulty):
+            end_time = time.time()
+            total_time = end_time - start_time
+
+            print("\nBlock mined")
+            print("Nonce:", nonce)
+            print("Hash:", hash_result)
+            print("Time taken (s):", round(total_time, 2))
+            print("Hash rate (hashes/sec):", int(attempts / total_time))
+            break
+
+        nonce += 1
+
+except KeyboardInterrupt:
+    print("\nMining stopped by user")
+    print("Total attempts:", attempts)
+
+
+
+
